50b0016710ef93b61b7415ca6209f0e9e1d47ad2
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-07 17:52:41 -0400

    Created constructor, set up state, tested by printing from state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,26 +2,26 @@ import React, { Components } from 'react';
 import Todo from './Todo';
 
 class TodoList extends Components {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             list: [],
             newTodo: "",
         }
     }
 
-    addTodo = (event) => {
-        const list = this.state.list;
-        const newTodo = this.state.newTodo;
-        list.push(newTodo);
-        this.setState({list: list, newTodo: ''});
-    }
+    // addTodo = (event) => {
+    //     const list = this.state.list;
+    //     const newTodo = this.state.newTodo;
+    //     list.push(newTodo);
+    //     this.setState({list: list, newTodo: ''});
+    // }
 
     render() {
-        return(
+        return (
             <div>
-                {this.state.list.map((item, index) => {
-                return(<Todo key={index} todo={item} />)}
+            <h1>Hi</h1>
+               {console.log("hi")}
             </div>
         );
     }
